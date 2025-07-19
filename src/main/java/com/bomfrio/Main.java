4bb04978cb7d05d9055997195b30a69917735b92
guutol <n4megustavo@gmail.com>
2025-07-19 17:43:06 -0300

    tela inicial criada utilizando Scene Builder

---
+++ src/main/java/com/bomfrio/Main.java
@@ -0,0 +1,23 @@
+package com.bomfrio;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/tela_inicial.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Bomfrio - Sistema de Ordens de Serviço");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
